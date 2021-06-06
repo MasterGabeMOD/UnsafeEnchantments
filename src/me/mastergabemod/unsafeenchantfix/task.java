3d5a289918afb0a0698e271fdd00811e52944a3d
Akutasan <mirodurana1@gmail.com>
2021-06-06 15:08:39 +0200

    Restyling, fixed minor bugs

--- src/me/mastergabemod/unsafeenchantfix/task.java
+++ src/me/mastergabemod/unsafeenchantfix/task.java
@@ -2,19 +2,14 @@ package me.mastergabemod.unsafeenchantfix;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import me.mastergabemod.unsafeenchantfix.main;
 
 
-public class task extends JavaPlugin
-{
+public class task extends JavaPlugin {
 
-    public task()
-    {
+    public task() {
+
     }
-    public void onEnable()
-    {
-    	
-Bukkit.getScheduler().runTaskTimer(this, new main(), 100L, 100L);
-
-}
+    public void onEnable() {
+        Bukkit.getScheduler().runTaskTimer(this, new main(), 100L, 100L);
+    }
 }
